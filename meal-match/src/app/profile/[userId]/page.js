5332d6a114854ab9db4208a6a57d288742ed02a0
pitbull51067 <pitbull51067@yahoo.com>
2025-04-17 01:42:53 -0400

    Revert "Removed unnecessary scrollbar on many pages"
    This reverts commit 91b2d3d28a715385d7db37eff0e68b3811662285.

--- meal-match/src/app/profile/[userId]/page.js
+++ meal-match/src/app/profile/[userId]/page.js
@@ -5,11 +5,11 @@ export default async function ProfilePage({ params }) {
     const { userId } = await params;
     
     return (
-        <div className="bg-gray-100 min-h-screen">
+        <>
             <LoggedInNav />
             <main className="relative">
                 <Profile userId={userId} />
             </main>
-        </div>
+        </>
     );
 }
